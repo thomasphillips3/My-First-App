415af9f4b2382535dd10dca0f4b0c9a1643d641e
Thomas Phillips <thomasphillips3@gmail.com>
2016-04-27 21:17:44 -0700

    Created intent to start second activity from button press

--- src/main/java/com/aspire/first/myfirstapp/DisplayMessageActivity.java
+++ src/main/java/com/aspire/first/myfirstapp/DisplayMessageActivity.java
@@ -1,12 +1,12 @@
 package com.aspire.first.myfirstapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -15,28 +15,23 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment()).commit();
-//        }
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
         return super.onOptionsItemSelected(item);
     }
 
     public static class PlaceholderFragment extends Fragment {
         public PlaceholderFragment() {}
-
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment.fragment_display_message, container, false);
-//            return rootView;
-//        }
     }
 }
